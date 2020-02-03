7f1d2cd4eaec1020a81a32b44aefdc761bd04f36
Rostyslav Toch <rokarostuk@gmail.com>
2020-02-03 23:56:39 +0200

    Added docker and go micro

---
+++ shippy-cli-consignment/go.mod
@@ -0,0 +1,5 @@
+module github.com/RostyslavToch/go-microservices/shippy-cli-consignment
+
+go 1.13
+
+require github.com/RostyslavToch/go-microservices v0.0.0-20200203192425-f7fb56473aca // indirect
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"context"
-	"log"
-	"net"
-	"sync"
+	"fmt"
 
 	pb "github.com/RostyslavToch/go-microservices/shippy-service-consignment/proto/consignment"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-)
-
-const (
-	port = ":50051"
+	"github.com/micro/go-micro"
 )
 
 type repository interface {
@@ -21,14 +14,10 @@ type repository interface {
 }
 
 type Repository struct {
-	mu           sync.RWMutex
 	consignments []*pb.Consignment
 }
 
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.mu.Lock()
-	defer repo.mu.Unlock()
-
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
@@ -42,33 +31,35 @@ type service struct {
 	repo repository
 }
 
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
 	consignment, err := s.repo.Create(req)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &pb.Response{Created: true, Consignment: consignment}, nil
+	res.Created = true
+	res.Consignment = consignment
+	return nil
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
 	consignments := s.repo.GetAll()
-	return &pb.Response{Consignments: consignments}, nil
+	res.Consignments = consignments
+	return nil
 }
 
 func main() {
 	repo := &Repository{}
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
 
-	s := grpc.NewServer()
-	pb.RegisterShippingServiceServer(s, &service{repo})
-	reflection.Register(s)
+	srv := micro.NewService(
+		micro.Name("shippy.service.consignment"),
+	)
 
-	log.Println("Running on port:", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	srv.Init()
+
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
 	}
 }
