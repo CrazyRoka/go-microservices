f04874bcf803efd6fc4b9dd059e9b42ee442fb15
Rostyslav Toch <rokarostuk@gmail.com>
2020-02-03 19:23:33 +0200

    first-commit

---
+++ shippy-cli-consignment/go.mod
@@ -0,0 +1,3 @@
+module github.com/RostyslavToch/shippy-cli-consignment
+
+go 1.13
---
+++ shippy-cli-consignment/main.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+
+}
